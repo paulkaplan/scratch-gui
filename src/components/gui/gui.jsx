87caef62064c92c63dfc8e4cc8cee836a215e549
Paul Kaplan <pkaplan@media.mit.edu>
2017-03-21 09:29:15 -0400

    Working manual tab setup

--- src/components/gui/gui.jsx
+++ src/components/gui/gui.jsx
@@ -1,6 +1,7 @@
 const React = require('react');
+const bindAll = require('lodash.bindAll')
 const VM = require('scratch-vm');
-
+const classNames = require('classnames');
 const Blocks = require('../../containers/blocks.jsx');
 const GreenFlag = require('../../containers/green-flag.jsx');
 const TargetPane = require('../../containers/target-pane.jsx');
@@ -11,61 +12,137 @@ const MenuBar = require('../menu-bar/menu-bar.jsx');
 const Box = require('../box/box.jsx');
 const styles = require('./gui.css');
 
-const GUIComponent = props => {
-    const {
-        basePath,
-        children,
-        vm,
-        ...componentProps
-    } = props;
-    if (children) {
-        return (
-            <Box {...componentProps}>
-                {children}
-            </Box>
-        );
+class GUIComponent extends React.Component {
+    constructor (props) {
+        super(props);
+        bindAll(this, [
+            'handleScriptTabSelect',
+            'handleSoundTabSelect',
+            'handleCostumeTabSelect'
+        ]);
+        this.state = {selectedTab: 'scripts'};
     }
-    return (
-        <Box
-            className={styles.pageWrapper}
-            {...componentProps}
-        >
-            <MenuBar />
-            <Box className={styles.bodyWrapper}>
-                <Box className={styles.flexWrapper}>
-                    <Box className={styles.blocksWrapper}>
-                        <Blocks
-                            grow={1}
-                            options={{
-                                media: `${basePath}static/blocks-media/`
-                            }}
-                            vm={vm}
-                        />
-                    </Box>
+    handleScriptTabSelect () {
+        this.setState({selectedTab: 'scripts'});
+    }
+    handleSoundTabSelect () {
+        this.setState({selectedTab: 'sounds'});
+    }
+    handleCostumeTabSelect () {
+        this.setState({selectedTab: 'costumes'});
+    }
+    render () {
+        const {
+            basePath,
+            children,
+            vm,
+            ...componentProps
+        } = this.props;
 
-                    <Box className={styles.stageAndTargetWrapper} >
-                        <Box className={styles.stageMenuWrapper} >
-                            <GreenFlag vm={vm} />
-                            <StopAll vm={vm} />
-                        </Box>
-                        
-                        <Box className={styles.stageWrapper} >
-                            <Stage
-                                shrink={0}
-                                vm={vm}
-                            />
+        if (children) {
+            return (
+                <Box {...componentProps}>
+                    {children}
+                </Box>
+            );
+        }
+
+        return (
+            <Box
+                className={styles.pageWrapper}
+                {...componentProps}
+            >
+                <MenuBar />
+                <Box className={styles.bodyWrapper}>
+                    <Box className={styles.flexWrapper}>
+                        <Box className={styles.editorWrapper}>
+                            <Box className={styles.tabList}>
+                                <Box
+                                    className={classNames(styles.tabListItem, {
+                                        [styles.activeTabListItem]: this.state.selectedTab === 'scripts'
+                                    })}
+                                    onClick={this.handleScriptTabSelect}
+                                >
+                                    Scripts
+                                </Box>
+                                <Box
+                                    className={classNames(styles.tabListItem, {
+                                        [styles.activeTabListItem]: this.state.selectedTab === 'costumes'
+                                    })}
+                                    onClick={this.handleCostumeTabSelect}
+                                >
+                                    Costumes
+                                </Box>
+                                <Box
+                                    className={classNames(styles.tabListItem, {
+                                        [styles.activeTabListItem]: this.state.selectedTab === 'sounds'
+                                    })}
+                                    onClick={this.handleSoundTabSelect}
+                                >
+                                    Sounds
+                                </Box>
+                            </Box>
+                            <Box className={styles.tabs}>
+                                <Box
+                                    className={classNames(styles.tabWrapper, {
+                                        [styles.activeTabWrapper]: this.state.selectedTab === 'scripts'
+                                    })}
+                                >
+                                    <Box className={styles.blocksWrapper}>
+                                        <Blocks
+                                            grow={1}
+                                            options={{
+                                                media: `${basePath}static/blocks-media/`
+                                            }}
+                                            vm={vm}
+                                        />
+                                    </Box>
+                                </Box>
+                                <Box
+                                    className={classNames(styles.tabWrapper, {
+                                        [styles.activeTabWrapper]: this.state.selectedTab === 'costumes'
+                                    })}
+                                >
+                                    <Box className={styles.costumesWrapper}>
+                                        <h1>Costumes</h1>
+                                    </Box>
+                                </Box>
+                                <Box
+                                    className={classNames(styles.tabWrapper, {
+                                        [styles.activeTabWrapper]: this.state.selectedTab === 'sounds'
+                                    })}
+                                >
+                                    <Box className={styles.soundsWrapper}>
+                                        <h1>Sounds</h1>
+                                    </Box>
+                                </Box>
+                            </Box>
                         </Box>
 
-                        <Box className={styles.targetWrapper} >
-                            <TargetPane
-                                vm={vm}
-                            />
+                        <Box className={styles.stageAndTargetWrapper} >
+                            <Box className={styles.stageMenuWrapper} >
+                                <GreenFlag vm={vm} />
+                                <StopAll vm={vm} />
+                            </Box>
+
+                            <Box className={styles.stageWrapper} >
+                                <Stage
+                                    shrink={0}
+                                    vm={vm}
+                                />
+                            </Box>
+
+                            <Box className={styles.targetWrapper} >
+                                <TargetPane
+                                    vm={vm}
+                                />
+                            </Box>
                         </Box>
                     </Box>
                 </Box>
             </Box>
-        </Box>
-    );
+        );
+    }
 };
 GUIComponent.propTypes = {
     basePath: React.PropTypes.string,
